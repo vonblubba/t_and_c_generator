7b66ac0ba557d2a51770aa3b6d1ac7dd3f6103c0
vonblubba <oscar.riva@gmail.com>
2022-06-08 13:56:45 +0200

    implement basic spec set

--- lib/generator.rb
+++ lib/generator.rb
@@ -2,6 +2,10 @@
 
 # Generates a document from a template and dataset
 class Generator
+  def initialize(template_path, clauses, sections)
+    # TODO: implement this
+  end
+
   def perform
     # TODO: implement this
   end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -3,5 +3,81 @@
 require 'generator'
 
 RSpec.describe Generator, '#perform' do
-  # TODO: implement specs
+  subject { Generator.new(template_path, clauses, sections) }
+
+  context 'when provided invalid clauses' do
+    let(:template_path) { 'spec/fixtures/files/template_00.txt' }
+    let(:clauses) { 'invalid' }
+    let(:expected_document) { 'error: invalid clauses' }
+    let(:sections) do
+      [
+        { 'id': 1, 'clauses_ids': [1, 2] }
+      ]
+    end
+
+    it 'returns invalid clause message' do
+      expect(subject.perform).to eq expected_document
+    end
+  end
+
+  context 'when provided invalid sections' do
+    let(:template_path) { 'spec/fixtures/files/template_00.txt' }
+    let(:clauses) do
+      [
+        { 'id': 1, 'text': 'The quick brown fox' },
+        { 'id': 2, 'text': 'jumps over the lazy dog' },
+        { 'id': 3, 'text': 'And dies' },
+        { 'id': 4, 'text': 'The white horse is white' }
+      ]
+    end
+    let(:expected_document) { 'error: invalid sections' }
+    let(:sections) { 'invalid' }
+
+    it 'returns invalid clause message' do
+      expect(subject.perform).to eq expected_document
+    end
+  end
+
+  context 'when clause in template is missing in dataset' do
+    let(:template_path) { 'spec/fixtures/files/template_00.txt' }
+    let(:clauses) do
+      [
+        { 'id': 2, 'text': 'jumps over the lazy dog' },
+        { 'id': 3, 'text': 'And dies' },
+        { 'id': 4, 'text': 'The white horse is white' }
+      ]
+    end
+    let(:expected_document) { 'error: undefined clause with id 1 in dataset' }
+    let(:sections) do
+      [
+        { 'id': 1, 'clauses_ids': [1, 2] }
+      ]
+    end
+
+    it 'generates undefined clause error' do
+      expect(subject.perform).to eq expected_document
+    end
+  end
+
+  context 'when provided valid template and dataset' do
+    let(:template_path) { 'spec/fixtures/files/template_00.txt' }
+    let(:clauses) do
+      [
+        { 'id': 1, 'text': 'The quick brown fox' },
+        { 'id': 2, 'text': 'jumps over the lazy dog' },
+        { 'id': 3, 'text': 'And dies' },
+        { 'id': 4, 'text': 'The white horse is white' }
+      ]
+    end
+    let(:expected_document) { File.read('spec/fixtures/files/expected_document_00.txt') }
+    let(:sections) do
+      [
+        { 'id': 1, 'clauses_ids': [1, 2] }
+      ]
+    end
+
+    it 'generates expected document' do
+      expect(subject.perform).to eq expected_document
+    end
+  end
 end
